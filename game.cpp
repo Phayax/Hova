82f500e2f3afca8a64d92ad1fd08d3e95954b5e6
Hexelena <phoenix.logd@gmx.de>
2016-06-11 14:06:56 +1200

    moved status text from window title to text rendered on screen  now showing velocity, rotation and ttt(ticks to target - the ticks that followMouseThrusters expects calculates in advance)

--- game.cpp
+++ game.cpp
@@ -7,6 +7,12 @@
 Game::Game()
 {
 	window.setFramerateLimit(60);
+	if (!font.loadFromFile("fonts/DroidSans.ttf")) {
+		window.close();
+		// cry (too lazy to figure out how to close the application)
+	}
+	statsText.setFont(font);
+	statsText.setCharacterSize(20);
 }
 
 void Game::run(){
@@ -14,7 +20,15 @@ void Game::run(){
 	{
 		window.clear(Color::Black);
 		if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
-		window.setTitle("Hova | mx: " + std::to_string(Mouse::getPosition(window).x) + " , my: " + std::to_string(Mouse::getPosition(window).y) + " | sx: " + std::to_string(ship.getPosition().x) + " , sy: " + std::to_string(ship.getPosition().y));
+		std::string statusString = 
+			"vel: " + std::to_string(ship.getVelocity().x) +
+			" ~ " + std::to_string(ship.getVelocity().y) +
+			//"mx: " + std::to_string(Mouse::getPosition(window).x) +
+			//" , my: " + std::to_string(Mouse::getPosition(window).y) + 
+			//" | sx: " + std::to_string(ship.getPosition().x) + 
+			//" , sy: " + std::to_string(ship.getPosition().y) + 
+			"\nrot: " + std::to_string(static_cast<int>(ship.getRotation()));
+		statsText.setString(statusString);
 		inputPhase();
 		updatePhase();
 		renderPhase();
@@ -27,7 +41,7 @@ void Game::inputPhase(){
 void Game::updatePhase(){
 	ship.applyGravity();
 	//ship.followMouseLine(Mouse::getPosition(window));
-	ship.followMouseThrusters(Mouse::getPosition(window));
+	statsText.setString(statsText.getString() + " | ttt: " + std::to_string(ship.followMouseThrusters(Mouse::getPosition(window))));
 	ship.update();
 	shipVector.update();
 }
@@ -35,5 +49,6 @@ void Game::updatePhase(){
 void Game::renderPhase(){
 	window.draw(ship.getShape());
 	window.draw(shipVector.getShape());
+	window.draw(statsText);
 	window.display();
 }
--- game.hpp
+++ game.hpp
@@ -15,6 +15,8 @@ private:
 	RenderWindow window{{windowWidth, windowHeight},"Hova - Fly Away!"};
 	Ship ship{windowWidth / 2, windowHeight / 2,100,40};
 	VelocityVector shipVector{&ship};
+	Text statsText{};
+	Font font{};
 
 public:
 	Game();
